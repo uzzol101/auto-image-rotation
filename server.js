d23bb4bc6db41f11f0ed93091ec9d1bac2f7b785
uzzol <davepillowgood@gmail.com>
2019-09-13 23:03:29 +0600

    client deleted

--- server.js
+++ server.js
@@ -2,23 +2,19 @@ const express = require('express')
 const app = express()
 const port = 4000
 const path = require('path')
-const fileUpload = require('express-fileupload')
+const multer  = require('multer')
+const upload = multer({ dest: 'upload/' })
+const cors = require('cors')
+const fs = require('fs')
+app.use(cors())
 
-app.use(fileUpload())
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/upload', (req, res) => {
-    console.log(req.files);
-    let sampleFile = req.files.sampleFile;
-
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(path.join(__dirname, 'upload'), function(err) {
-      if (err)
-        return res.status(500).send(err);
-  
-      res.send('File uploaded!');
-    });
-})
+app.post('/upload', upload.single('sampleFile'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    res.send({message:"file uploaded"})
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
