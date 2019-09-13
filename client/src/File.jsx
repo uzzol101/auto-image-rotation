f21b994f62b56b13f71980f97f5b82f591302a7f
uzzol <davepillowgood@gmail.com>
2019-09-14 00:28:23 +0600

    converting to file working

--- client/src/File.jsx
+++ client/src/File.jsx
@@ -10,20 +10,34 @@ export default class File extends Component {
 
     handleFileChange = (event) => {
         const file = event.target.files[0]
+        console.log('file ', file)
         const imageUrl = URL.createObjectURL(file)
-        const fomrData = new FormData()
-        fomrData.append("sampleFile", file)
-        fomrData.append("name", "uzzol")
-        this.setState({
-            file: imageUrl
-        })
-        window.fetch('http://localhost:4000/upload', {
-            method: "POST",
-            body: fomrData
-        })
-        .then(response => response.json())
-        .then(result => console.log('result ', result))
-        .catch(error => console.log('error', error))
+        let fileBlob
+        window.fetch(imageUrl).then(response => response.blob()).then(result => {
+            
+            result.name = "sampleFile"
+            result.lastModifiedDate = new Date()
+            fileBlob = result
+            const fd = new FormData();
+            fd.set('a', fileBlob, file.name);
+            let f =  fd.get('a');
+            console.log('f ', f)
+            const fomrData = new FormData()
+            fomrData.append("sampleFile", f)
+            fomrData.append("name", "uzzol")
+            this.setState({
+                file: imageUrl
+            })
+            window.fetch('http://localhost:4000/upload', {
+                method: "POST",
+                body: fomrData
+            })
+            .then(response => response.json())
+            .then(result => console.log('result ', result))
+            .catch(error => console.log('error', error))
+            })
+        
+        
     }
 
     render() {
